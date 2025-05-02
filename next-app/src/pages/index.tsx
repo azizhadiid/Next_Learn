0f4e3f7bbcccb54aa06d34403aaca4d69cf12792
azizhadiid <azizalhadiid88@gmail.com>
2025-05-02 19:57:41 +0700

    Set Up Next JS Version 13.4.7

---
+++ next-app/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Hello Aziz</h1>
+      </div>
+    </div>
+  )
+}
